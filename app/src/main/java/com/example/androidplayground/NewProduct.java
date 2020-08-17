ef297ad3c9242a48e6f9bd25f0493018efa39807
animalSounds-ldn <animalsounds.ldn@gmail.com>
2020-08-17 08:57:33 +0100

    building second screen

---
+++ app/src/main/java/com/example/androidplayground/NewProduct.java
@@ -0,0 +1,13 @@
+package com.example.androidplayground;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewProduct extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newproduct);
+    }
+}
